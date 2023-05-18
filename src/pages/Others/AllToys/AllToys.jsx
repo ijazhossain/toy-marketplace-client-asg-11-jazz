fdf3f75541f99dd3939d5cfd75381b2277a07760
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-19 03:47:19 +0600

    all toys page && search by toy name added

--- src/pages/Others/AllToys/AllToys.jsx
+++ src/pages/Others/AllToys/AllToys.jsx
@@ -1,9 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Spinner from '../../Shared/Spinner/Spinner';
+/* ==============================
+        All Toys Page
+================================= */
 
 const AllToys = () => {
+    const [toys, setToys] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [searchText, setSearchText] = useState('')
+
+    // console.log(imageUrl, toyName, sellerName, sellerEmail, category, price, quantity, ratings, description);
+    const handleSearch = () => {
+        fetch(`http://localhost:5000/getToysByName/${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setToys(data)
+            })
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch('http://localhost:5000/allToys')
+            const toysData = await response.json()
+            console.log(toysData);
+            setToys(toysData)
+            setLoading(false)
+
+
+        }
+        fetchData()
+    }, [])
+    if (loading) {
+        return <Spinner></Spinner>
+    }
+    // console.log(searchText);
     return (
-        <div>
-            <h1>This is all toys page</h1>
+        <div className='w-[80%] mx-auto'>
+            <div className='relative w-1/2 mx-auto my-12'>
+                <input onChange={e => setSearchText(e.target.value)} className='w-full h-12 border border-[#874b30] rounded-xl text-[#874b30] px-5' type="search" name="" id="" placeholder='Search By Name' />
+                <button onClick={handleSearch} className='absolute top-0 right-0 h-12 bg-[#874b30] rounded-s-none rounded-e-xl px-5 text-white'>Search</button>
+            </div>
+            <div>
+
+                <table className="table table-zebra w-full ">
+                    <thead >
+                        <tr >
+                            <th className='bg-[#874b30] text-white' >#</th>
+                            <th className='bg-[#874b30] text-white'>Seller</th>
+                            <th className='bg-[#874b30] text-white'>Toy name</th>
+                            <th className='bg-[#874b30] text-white'>Sub-Category</th>
+                            <th className='bg-[#874b30] text-white'>Price</th>
+                            <th className='bg-[#874b30] text-white'>Quantity</th>
+                            <th className='bg-[#874b30] text-white'></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            toys.map((toy, index) => <tr key={toy._id}>
+                                <th>{index + 1}</th>
+                                <td>{toy.sellerName}</td>
+                                <td>{toy.toyName}</td>
+                                <td>{toy.category}</td>
+                                <td>${toy.price}</td>
+                                <td>{toy.quantity}</td>
+                                <td><button className='ml-5 font-semibold  bg-[#874b30] text-white rounded-lg h-[40px] px-5 '>Details</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+
+
+            </div>
         </div>
     );
 };
