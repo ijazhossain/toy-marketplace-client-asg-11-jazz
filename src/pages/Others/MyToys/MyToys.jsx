f9801d9f96853a405ada52e9c2c5ee05ad7b5928
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-20 00:28:15 +0600

    adding functionality to delete toy btn

--- src/pages/Others/MyToys/MyToys.jsx
+++ src/pages/Others/MyToys/MyToys.jsx
@@ -2,14 +2,36 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../../Providers/AuthProvider';
 import Spinner from '../../Shared/Spinner/Spinner';
 import Modal from '../Modal/Modal';
+import Swal from 'sweetalert2';
 
-
+/* =============================
+        MY Toys Page
+=================================*/
 const MyToys = () => {
     const { user } = useContext(AuthContext)
     const [myToyDetails, setMyToyDetails] = useState([])
     const [loading, setLoading] = useState(true)
     const [reload, setReload] = useState(0)
-
+    const handleDelete = (_id) => {
+        console.log(_id);
+        fetch(`http://localhost:5000/toy/${_id}`, {
+            method: 'DELETE'
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Success',
+                        text: 'Toy deleted successfully ',
+                        footer: '<a href="">Why do I have this issue?</a>'
+                    })
+                    const remaining = myToyDetails.filter(myToy => myToy._id !== _id);
+                    setMyToyDetails(remaining);
+                }
+            })
+    }
 
     useEffect(() => {
         fetch(`http://localhost:5000/myToy?email=${user?.email}`)
@@ -48,7 +70,7 @@ const MyToys = () => {
                         myToyDetails.map(myToy => <tr key={myToy._id}>
                             <th>
                                 <label>
-                                    <button className="btn btn-circle btn-sm bg-[#874b30]">
+                                    <button onClick={() => handleDelete(myToy._id)} className="btn btn-circle btn-sm bg-[#874b30]">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
                                     </button>
 
