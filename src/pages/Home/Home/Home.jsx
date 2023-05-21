39c4683732fea75875b6abf9abd98c8001d8783f
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-21 22:12:20 +0600

    adding AOS package to homepage

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Banner from '../Banner/Banner';
 import Category from '../Category/Category';
 import ImageGallery from '../ImageGallery/ImageGallery';
 import Intro from '../Intro/Intro';
 import About from '../About/About';
 import useTitle from '../../../hooks/useTitle';
-
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 
 const Home = () => {
     useTitle('Home')
+    useEffect(() => {
+        Aos.init();
+    }, [])
     return (
         <div>
             <Banner></Banner>
-            <Intro></Intro>
+            <div data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="3000"
+                data-aos-anchor-placement="center-center"
+            >
+                <Intro></Intro>
+            </div>
             <Category></Category>
             <About></About>
             <ImageGallery></ImageGallery>
