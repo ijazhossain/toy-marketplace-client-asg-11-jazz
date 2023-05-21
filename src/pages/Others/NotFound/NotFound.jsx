af25be2b72ac87d8fc8fd6a328bb62c957fe6a1e
ijaz35 <ijaz.hossain1@gmail.com>
2023-05-21 19:57:17 +0600

    error page added

--- src/pages/Others/NotFound/NotFound.jsx
+++ src/pages/Others/NotFound/NotFound.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import errImg from '../../../assets/images/error.png'
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-            <h1 className="text-6xl">Page not found</h1>
+        <div className='flex flex-col items-center justify-center bg-[#f4f1ec] h-screen'>
+            <img className='w-[70%] lg:w-[35%]' src={errImg} alt="error img" />
+            <button onClick={() => navigate('/')} className='mt-12 border-0 capitalize text-[17px] bg-[#8b6753] btn btn-wide '>Back To Home</button>
         </div>
     );
 };
